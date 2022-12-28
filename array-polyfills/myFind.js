3015485ae4405c1cf1911eb22e40a7eceb6309b6
Siam Rahman <siamr.bro99@gmail.com>
2022-12-28 02:30:34 -0500

    Delete array-polyfills directory

--- array-polyfills/myFind.js
+++
@@ -1,8 +0,0 @@
-const pets = ["bunny", "hamster", "dog", "cat", "lizard", "parrot", "fish", "hamster", "cat", "parrot", "hamster", "dog"];
-
-Array.prototype.myFind = function(callback) {
-    for (let i = 0; i < this.length; i++) {
-        if (callback(this[i])) return this[i];
-    }
-    return undefined;
-}
